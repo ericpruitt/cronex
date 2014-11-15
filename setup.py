49103685a8786e7df7245e8313ed5a8e4cff1391
Eric Pruitt <eric.pruitt@gmail.com>
2014-11-14 16:35:06 -0800

    Removed first name from LICENSE and setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='cronex',
       version='0.1.0',
       description='This module provides an easy to use interface for cron-like task scheduling.',
-      author='James (Eric) Pruitt',
+      author='Eric Pruitt',
       author_email='eric.pruitt@gmail.com',
       url='https://github.com/ericpruitt/cronex',
       py_modules=['cronex'],
