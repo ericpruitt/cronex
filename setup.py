80f94a40d361ba29b386ca99ae1d4694545628dd
Eric Pruitt <eric.pruitt@gmail.com>
2014-11-16 15:18:32 -0800

    Prepared setup.py for PyPI upload
    - The setup.py file has been modified to include classifier information,
      and if Pandoc is available, it will convert the README to REstructured
      Text so that it can be shown on the PyPI page.

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
-from distutils.core import setup
+import distutils.core
+import subprocess
 
-setup(name='cronex',
-      version='0.1.0',
-      description='This module provides an easy to use interface for cron-like task scheduling.',
-      author='Eric Pruitt',
-      author_email='eric.pruitt@gmail.com',
-      url='https://github.com/ericpruitt/cronex',
-      packages=['cronex'],
-     )
+# If pandoc is available, convert the markdown README to REstructured Text.
+try:
+    pandoc = subprocess.Popen(["pandoc", "-t", "rst", "README.md"],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+    long_description, _ = pandoc.communicate()
+except OSError:
+    long_description = None
+
+distutils.core.setup(
+    name="cronex",
+    version="0.1.0",
+    description=("This module provides an easy to use interface for cron-like"
+                 " task scheduling."),
+    author="Eric Pruitt",
+    author_email="eric.pruitt@gmail.com",
+    url="https://github.com/ericpruitt/cronex",
+    license="MIT",
+    keywords=["cron", "scheduler", "triggers", "time", "quartz"],
+    packages=["cronex"],
+    long_description=long_description,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+)
