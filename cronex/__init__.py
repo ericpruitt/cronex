4830988a01dc33554724c8e0fca3479900bebfbc
Michael Dunn <michael.dunn@realself.com>
2018-10-25 15:34:33 -0700

    change to isdigit()

--- cronex/__init__.py
+++ cronex/__init__.py
@@ -298,14 +298,6 @@ def is_special_atom(cron_atom, span):
         return False
 
 
-def string_is_int(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-    
-
 def parse_atom(parse, minmax):
     """
     Returns a set containing valid values for a given cron-style range of
@@ -348,7 +340,7 @@ def parse_atom(parse, minmax):
             prefix, suffix = [int(n) for n in subrange.split('-')]
             if prefix < minmax[0] or suffix > minmax[1]:
                 raise ValueError("\"%s\" is not within valid range." % parse)
-        elif string_is_int(subrange):
+        elif subrange.isdigit():
             # Handle offset increments e.g. 5/15 to run at :05, :20, :35, and :50
             return set(xrange(subrange,minmax[1] + 1), increment)
         elif subrange == '*':
